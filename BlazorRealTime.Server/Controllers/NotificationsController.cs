13e0f77b9adfa075e23b6bd5df5795789f37bc20
Emilien Kopp <emilien.kopp@outlook.com>
2021-11-06 02:12:43 +0900

    Add project files.

---
+++ BlazorRealTime.Server/Controllers/NotificationsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorRealTimeApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+
+        //The service.addsignalR automatically injects this context
+        private readonly IHubContext<NotificationHub> _hubContext;
+        
+        public NotificationsController(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromQuery] string title)
+        {
+            await _hubContext.Clients.All.SendAsync("notification", $"{DateTime.Now} : {title}");
+            return Ok("Notification has been sent successfully");
+        }
+
+    }
+}
